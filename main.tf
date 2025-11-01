edb84a5a8c76f18244f4ea3d67cfabab0866537c
Harry Warring <harjinder90singh@gmail.com>
2025-11-01 12:52:19 -0400

    All files added

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.region
+}
+
+resource "aws_instance" "myserver" {
+  ami = "ami-0199d4b5b8b4fde0e"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "SampleServer"
+  }
+}
